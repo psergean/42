1c91b3eb045178c6cca5a73d62289a2a51301186
SERGEANT Pierre <sergeantpierre@MacBook-Pro-de-SERGEANT.local>
2017-10-06 04:26:44 +0200

    OnTheRoadAgain

--- Fract_ol/srcs/display.c
+++ Fract_ol/srcs/display.c
@@ -22,10 +22,11 @@ void		display(t_env *env)
 	mlx_string_put(env->mlx, env->win, 10, 100, 0x00FFFFFF, "Dendrite : n");
 	mlx_string_put(env->mlx, env->win, 10, 120, 0x00FFFFFF, "Julia_bis : k");
 	mlx_string_put(env->mlx, env->win, 10, 140,
-			0x00FFFFFF, "Move : ^ : v : < : >");
+			0x00FFFFFF, "Move : ^ , v , < , >");
 	mlx_string_put(env->mlx, env->win, 10, 160,
-			0x00FFFFFF, "Iteration : i : o");
-	mlx_string_put(env->mlx, env->win, 10, 180, 0x00FFFFFF, "Zoom : + : -");
+			0x00FFFFFF, "Iteration : i , o");
+	mlx_string_put(env->mlx, env->win, 10, 180, 0x00FFFFFF, "Zoom : + , -");
 	mlx_string_put(env->mlx, env->win, 10, 200, 0x00FFFFFF, "Reset : r");
-	mlx_string_put(env->mlx, env->win, 10, 220, 0x00FFFFFF, "Quit : Esc");
+	mlx_string_put(env->mlx, env->win, 10, 220, 0x00FFFFFF, "Colors RGB: 1, 2, 3");
+	mlx_string_put(env->mlx, env->win, 10, 240, 0x00FFFFFF, "Quit : Esc");
 }

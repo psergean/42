dc95e196ed0ee5f37ef3160876559d0fd28aa9e1
Pierre SERGEANT <psergean@e3r4p6.42.fr>
2018-01-09 21:35:49 +0100

    OnTheRoadAgain

--- Piscine_PHP/Day01/ex05/epur_str.php
+++ Piscine_PHP/Day01/ex05/epur_str.php
@@ -1,13 +1,20 @@
 #!/usr/bin/php
 <?PHP
 
+function filter($var)
+{
+  if ($var == NULL)
+    return (false);
+  return (true);
+}
+
 if ($argc == 2)
 {
   $str = $argv[1];
   $str = trim($str);
   if ($str == NULL)
     return (false);
-  $tab = array_filter(explode(' ', $str));
+  $tab = array_filter(explode(' ', $str), filter);
   $tab = implode(' ', $tab);
   echo "$tab\n";
 }

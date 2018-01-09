fa38b9a3da234de980be81c84ca7d6d89f5b9970
Pierre SERGEANT <psergean@e3r4p6.42.fr>
2018-01-09 21:40:54 +0100

    OnTheRoadAgain

--- Piscine_PHP/Day01/ex03/ft_split.php
+++ Piscine_PHP/Day01/ex03/ft_split.php
@@ -1,11 +1,18 @@
 #!/usr/bin/php
 <?PHP
 
+function filter($var)
+{
+  if ($var == NULL)
+    return (false);
+  return (true);
+}
+
 function ft_split($str)
 {
   if ($str == NULL)
     return (false);
-  $tab = array_filter(explode(' ', $str));
+  $tab = array_filter(explode(' ', $str), filter);
   sort($tab);
   return ($tab);
 }
